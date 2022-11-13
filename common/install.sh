4d2c2fecb27d1e1c83909c9e77f06056cc90c9c5
Jai <jairajpoojary08@gmail.com>
2022-11-13 18:39:23 +0530

    A13_update

--- common/install.sh
+++ common/install.sh
@@ -2,13 +2,8 @@
 #Write your custom functions here
 #Remove this and Add your own custom texts here
 ui_print "  " 
-ui_print "  " 
 ui_print "********************************" 
 ui_print "  Installing Patches.... " 
 ui_print "********************************"
-#Do something fancy here XD
-   ui_print "  " 
-   ui_print " Extracting Files, please wait.... . "
-   ui_print "  " 
-   #add your name inside any ui print
-   #add custom functions if u need
+ui_print " Extracting Files, please wait.... . "
+#add custom functions if u need
