2d592964522d6fa2a38548a651267f127e734ab3
DrDisagree <crazymahmud08@gmail.com>
2023-07-08 16:05:20 +0600

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -1,9 +1,11 @@
-#Scripts by @Jai_08
-#Write your custom functions here
-#Remove this and Add your own custom texts here
-ui_print "  " 
-ui_print "********************************" 
-ui_print "  " 
-ui_print "********************************"
-ui_print " Extracting Files, please wait.... . "
-#add custom functions if u need
+# Scripts by @Jai_08 & @DrDisagree
+# Write your custom functions here
+# Remove this and Add your own custom texts here
+
+ui_print "  "
+ui_print "- Extracting files"
+ui_print "  "
+ui_print "- Please wait..."
+ui_print "  "
+
+# Add custom functions if u need
